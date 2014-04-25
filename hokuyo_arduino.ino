7c99f71d4febd18d5858f823750de613e3b8a0f7
jordi <jordi.solerbusquets@gmai.com>
2014-04-25 16:20:36 +0200

    Esquema principal V3

--- hokuyo_arduino.ino
+++ hokuyo_arduino.ino
@@ -8,7 +8,9 @@
 *
 * Compila, provat en arduino, sense PAP.
 *
-* 2-04-2014
+* Vermell, blau, verd, negre
+*
+* 4-04-2014
 */
 
 
@@ -16,26 +18,47 @@
 #include <ros/time.h>
 #include <lidar_scan/CodiGir.h>
 #include <std_msgs/Empty.h>
-#include "TimerOne.h"
+#include <TimerOne.h>
 
-#define entradaOpb A0
+#define entradaOpb A5
 #define duradaPols 2
 #define periodeDef 1250 //800Hz
 
-const int bot = 13; //Pin de STEP
-const int dir = 7;  //Pin de direccio
-const int obp = 8;  //Pin del díode del sensor fotoelèctric.
-//const int limlectura = 40;
-//int lecturaOpb;
+//Representació simbòlica dels pins conectats al driver i al sensor òptic (obp).
+const int p_dir = 13;
+const int p_bot = 12;
+const int p_sleep = 11;
+const int p_reset = 10;
+const int p_m2 = 9;
+const int p_m1 = 8;
+const int p_m0 = 7;
+const int p_enable = 6;
+const int p_fault = 5;
+const int p_obp = 4;
+
+const int limlectura = 240;
+int lecturaOpb;
 int num;
 int perScan;
+bool casa;
+
+void performStep(){
+    digitalWrite(p_bot, HIGH);
+    delayMicroseconds(duradaPols);
+    digitalWrite(p_bot, LOW);
+    num++;
+}
 
 void configurar(const lidar_scan::CodiGir& missatge){
-  Timer1.stop();
-  Timer1.detachInterrupt();
-  perScan = 25000/missatge.per;
-  Timer1.initialize(missatge.per);
-  Timer1.attachInterrupt(performStep);
+    Timer1.stop();
+    Timer1.detachInterrupt();
+    if(!casa){
+        homing();
+    }
+    perScan = 25000/missatge.per;
+    Timer1.initialize(missatge.per);
+    Timer1.attachInterrupt(performStep);
+    casa = false;
 }
 
 std_msgs::Empty bota_msg;
@@ -45,36 +68,76 @@ ros::Subscriber<lidar_scan::CodiGir> config_sub("girConfig", &configurar );
 ros::Publisher flanc("/home", &flanc_msg);
 ros::Publisher bota("/syncPap", &bota_msg);
 
-
-
-void setup(){
-  pinMode(bot, OUTPUT);
-  //pinMode(obp, OUTPUT);
-  pinMode(dir, OUTPUT);
-  digitalWrite(dir, LOW);
-  nh.initNode();
-  nh.subscribe(config_sub);
-  nh.advertise(bota);
-  nh.advertise(flanc);
-  num = 0;
-  perScan = 25000/periodeDef;
+void homing(){
+    int i;
+    lecturaOpb=analogRead(entradaOpb);
+    while(lecturaOpb > limlectura && i < 40){
+        performStep();
+        lecturaOpb=analogRead(entradaOpb);
+        delay(10);
+        if(lecturaOpb > limlectura){
+            i++;
+        }
+    }
+    /*
+    lecturaOpb=analogRead(entradaOpb);
+    if(lecturaOpb>limlectura){
+        for(int i=0; i<40; i++){
+            performStep();
+            delay(2);
+        }
+        
+    }*/
+    while(lecturaOpb < limlectura){
+        performStep();
+        lecturaOpb=analogRead(entradaOpb);
+        delay(2);
+    }
+    
+    num=0;
+    casa = true;
+    flanc.publish(&flanc_msg);
 }
 
-void performStep(){
-  digitalWrite(bot, HIGH);
-  delayMicroseconds(duradaPols);
-  digitalWrite(bot, LOW);
-  num++;
+void setup(){
+    pinMode(p_dir, OUTPUT);
+    pinMode(p_bot, OUTPUT);
+    pinMode(p_sleep, OUTPUT);
+    pinMode(p_reset, OUTPUT);
+    pinMode(p_m2, OUTPUT);
+    pinMode(p_m1, OUTPUT);
+    pinMode(p_m0, OUTPUT);
+    pinMode(p_enable, OUTPUT);
+    pinMode(p_fault, INPUT);
+    pinMode(p_obp, OUTPUT);
+
+    digitalWrite(p_dir, LOW);
+    digitalWrite(p_sleep, HIGH);
+    digitalWrite(p_reset, HIGH);
+    digitalWrite(p_m2, HIGH);
+    digitalWrite(p_m1, HIGH);
+    digitalWrite(p_m0, HIGH);
+    digitalWrite(p_enable, LOW);
+    digitalWrite(p_obp, HIGH);
+
+    nh.initNode();
+    nh.subscribe(config_sub);
+    nh.advertise(bota);
+    nh.advertise(flanc);
+    perScan = 25000/periodeDef;
+    //homing();
+    num=0;
 }
 
 void loop(){
-  if(num > perScan){
-      bota.publish(&bota_msg);
-      num = 0;
-  }
-  //lecturaOpb=analogRead(entradaOpb);
-  //if(lecturaOpb > limlectura){
-  //    flanc.publish(&flanc_msg);
-  //}
-  nh.spinOnce();
+    if(num > perScan){
+        bota.publish(&bota_msg);
+        num = 0;
+        lecturaOpb=analogRead(entradaOpb);
+        if(lecturaOpb > limlectura){
+            flanc.publish(&flanc_msg);     
+        }
+    }
+    
+    nh.spinOnce();
 }
--- lidar_scan_node.cpp
+++ lidar_scan_node.cpp
@@ -28,94 +28,99 @@
 #define periodeDefecte 1500
 #define maxPeriode 25000	// hokuyo: 40s/volta -> un escaneig per step
 #define minPeriode 1250		// hokuyo: 2s/volta
+#define blinding 0.7
 using namespace std;
 
 const int inca = 15.625*M_PI/periodeDefecte;
+static const ros::Duration aCegues(blinding);
+
 class estatPap
 {
 public:
 	bool publicable, preparada;
 
-	estatPap()
-	{
+	estatPap()	{
 		angle = angleInicial;
 		publicable = false;
 		preparada = false;
 		dAngle = inca;
 	}
 
-	estatPap(double delta)
-	{
+	estatPap(double delta)	{
 		angle = angleInicial;
 		publicable = false;
 		preparada = false;
 		dAngle = delta;
 	}
 
-	void resetejar()
-	{
+	void resetejar()	{
 		publicable = false;
 		preparada = false;
 	}
 
-	void preparar()
-	{
+	void preparar()	{
 		angle = angle + dAngle;
 		preparada = true;
 	}
 
-	tf::Transform transform()
-	{
+	tf::Transform transform()	{
 		tf::Transform tr;
 		tf::Quaternion q;
-		q.setRPY(angle*2, -M_PI/2, 0);
+		q.setRPY(-angle*2, -M_PI/2, 0);
 		tr.setRotation(q);
 		tr.setOrigin( tf::Vector3(coorx, coory, coorz) );
 		return tr;
 	}
 
-	void timeOut(const std_msgs::Empty& msg)
-	{
+	void timeOut(const std_msgs::Empty& msg)	{
 		publicable = true;
+	}
+	
+	void casa(const std_msgs::Empty& msg)	{
+		if(ros::Time::now() > darrerFlanc+aCegues){
+			angle = angleInicial;
+			darrerFlanc = ros::Time::now();
+		}
 	}
 private:
 
 	double angle, dAngle;
+	ros::Time darrerFlanc;
 };
 
-void publicar(tf::Transform tr)
-{
+
+void publicar(tf::Transform tr){
 	static tf::TransformBroadcaster emisor;
 	emisor.sendTransform(tf::StampedTransform(tr, ros::Time::now() + ros::Duration(tempsOffset), "base_laser", "hokuyo"));
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
 	int periode;
+	int periodeNou;
 	
 	ros::init(argc, argv, "lidar_scan_node");
 	ros::NodeHandle n;
 	ros::NodeHandle nh("~");
 	ros::Publisher gir_pub = n.advertise<lidar_scan::CodiGir>("/girConfig", 10);
 	
-	//ros::Subscriber flanc_sub;
 	tf::Transform t;
 	lidar_scan::CodiGir gir_msg;
+	
 
-	if (nh.getParam("periode", periode))
-	{
+	if (nh.getParam("periode", periode)){
 		if(periode < minPeriode){
 			periode = minPeriode;
+			nh.setParam("periode", minPeriode);
 			ROS_INFO("El periode de stepping ha de ser, com a minim: %u. Aquest ha estat el que s'ha establert.", minPeriode);
 		}else if(periode > maxPeriode){
 			periode = maxPeriode;
+			nh.setParam("periode", maxPeriode);
 			ROS_INFO("El periode de stepping ha de ser, com a maxim: %u. Aquest ha estat el que s'ha establert.", maxPeriode);
 		}else{
 			ROS_INFO("S'ha inicilitzat l'escaneig al periode: %u", periode);
 		}
 		
-	}else
-	{
+	}else	{
 		periode = periodeDefecte;
 		ROS_INFO("S'ha inicilitzat l'escaneig al periode per defecte: %u", periode);
 	}
@@ -123,6 +128,7 @@ int main(int argc, char **argv)
 	double increment = 15.625*M_PI/periode;
 	estatPap estat = estatPap(increment);
 	ros::Subscriber bot_sub = n.subscribe("/syncPap", 5, &estatPap::timeOut, &estat);
+	ros::Subscriber flanc_sub = n.subscribe("/home", 5, &estatPap::casa, &estat) ;
 	gir_msg.per = periode;
 	
 	int nio=0;
@@ -132,19 +138,43 @@ int main(int argc, char **argv)
 		nio++;
 	}
 	
-	while (ros::ok())
-	{
-		if(!estat.preparada)
-		{
+	while (ros::ok())	{
+		if(!estat.preparada){
 			estat.preparar();
 			t = estat.transform();
 		}
-		if(estat.publicable)
-		{
+		if(estat.publicable){
 			publicar(t);
 			estat.resetejar();
 		}
 
+		if(nh.getParam("periode", periodeNou)){
+			if(periodeNou != periode){
+				if(periodeNou < minPeriode){
+					periode = minPeriode;
+					nh.setParam("periode", minPeriode);
+					ROS_INFO("El periode de stepping ha de ser, com a minim: %u. Aquest ha estat el que s'ha establert.", minPeriode);
+				}else if(periodeNou > maxPeriode){
+					periode = maxPeriode;
+					nh.setParam("periode", maxPeriode);
+					ROS_INFO("El periode de stepping ha de ser, com a maxim: %u. Aquest ha estat el que s'ha establert.", maxPeriode);
+				}else{
+					periode = periodeNou;
+					ROS_INFO("S'ha canviat el periode d'escaneig a: %u", periode);
+				}
+				double increment = 15.625*M_PI/periode;
+				estat = estatPap(increment);
+					
+				gir_msg.per = periode;
+				
+				int nio=0;
+				while(ros::ok() && nio<=1){
+					sleep(1);
+					gir_pub.publish(gir_msg);
+					nio++;
+				}
+			}
+		}
 		ros::spinOnce();
 	}
 
